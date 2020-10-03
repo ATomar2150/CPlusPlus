2b42edebb718f206736470f35a6e9db7832f95c0
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-10-02 21:39:57 -0400

    IMP> PRIME NUMBERS

--- isPrimeFunction/main.cpp
+++ isPrimeFunction/main.cpp
@@ -1,53 +1,49 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void primeNumber(long);
+bool isPrime(int);
 
 int main()
 {
-    long number;
-    cout<< "This program tests if the number is PRIME OR NOT."<<endl;
-   
-    do{
-        cout<<"Please enter the number,it will run once but if you want to quit then press -1.\n";  
-        cin>> number;
-        cout<<"The number is: "<<number<<endl;
-        if(number==1 && number==2 && number==3 && number==7 && number==13){
-            cout<< "The number is prime.\n";
-            exit(EXIT_SUCCESS);
-        }
-        primeNumber(number);
-        }while(number != -1);
-        return 0;
-}
+    ofstream outputFile;
+    outputFile.open("primenumber.txt");
 
-void primeNumber(long number)
-{
-    if (number % 2 ==0)
-    {
-        cout<<"Its is not a prime number, as it is divisible by 2.\n";
-    }
-    else if(number % 3 == 0)
-    {
-        cout<<"Its is not a prime number, as it is divisible by 3.\n";
-    }
-     else if(number % 7 == 0)
-    {
-        cout<<"Its is not a prime number, as it is divisible by 7.\n";
-    }
-     else if(number % 11 == 0)
-    {
-        cout<<"Its is not a prime number, as it is divisible by 11.\n";
-    }
-    else if((number % 13 == 0))
-    {
+    int number;
 
-        cout<<"The number is not prime, as it is divisible by 13.\n";
+    if(outputFile)
+    {
+        for(int number=1; number<= 100; number++)
+        {
+            if(isPrime(number))
+            {
+            cout<<"The number is: "<<number<<". It is prime\n";
+            }
+            else
+            {
+                cout<<"The number is: "<<number<<". It is non prime\n";
+            }
+        outputFile.close();
+         }
     }
     else
     {
-        cout<<"The number is prime\n";
-    }
+        cout<<"Error opening in file.\n";
+    }  
+ 
+    return 0;
     
 }
 
+bool isPrime(int number)
+{
+    int isPrime =0;
+    for(int i=1; i<=number; i++)
+    {
+         if(number%i==0)
+         {
+             isPrime++;
+         }
+    }
+   return isPrime == 2 ? 1 : 0;
+}
