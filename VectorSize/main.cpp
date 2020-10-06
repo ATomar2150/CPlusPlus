e79228cbff18e2fc8d3bb73493e20d999c210a26
ATomar2150 <56009926+ATomar2150@users.noreply.github.com>
2020-10-06 01:53:38 -0400

    additional programs

--- VectorSize/main.cpp
+++ VectorSize/main.cpp
@@ -13,12 +13,33 @@ int main()
         values.push_back(i*2);
     }
     showValues(values);
+
+
+    values.pop_back();
+    cout<<"We are going to delete the last value by using pop function.\n";
+    cout<<"==============================================================="<<endl;
+    showValues(values);
+    cout<<"We are going to clear all th values by using clear function.\n";
+    cout<<"==============================================================="<<endl;
+    values.clear();
+    showValues(values);
+    cout<<"Values are cleared off\n";
     return 0;
     
 }
 void showValues(vector<int> vect)
 {
-    for(int i = 0; i < vect.size(); i++){
+    //checking if values is an empty array or not.
+    if(vect.empty())
+    {
+        cout<<"No value\n";
+    }
+    else
+    {
+    for(int i = 0; i < vect.size(); i++)
+    {
         cout<<vect[i]<<endl;
     }
+    }
+    
 }
